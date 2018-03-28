b25196c76de3ebeaa8f71aa65e2ce41da5607ab8
unknown <syyadkhan@yahoo.com>
2018-03-27 21:17:21 -0400

    mar271917

--- slp.js
+++ slp.js
@@ -1,21 +1,21 @@
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const container = document.querySelector('.container');
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".container");
 
-left.addEventListener('mouseenter', () =>{
-    container.classList.add('hover-left');
+left.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
 });
 
-left.addEventListener('mouseleave', () =>{
-    container.classList.remove('hover-left');
+left.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
 });
 
-right.addEventListener('mouseenter', () =>{
-    container.classList.add('hover-right');
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
 });
 
-right.addEventListener('mouseleave', () =>{
-    container.classList.remove('hover-right');
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
 });
 
 
